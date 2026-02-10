abec029520f18e3390a1d33731d333ea7549d2d3
girirajbidwai <bidwaigiriraj7@gmail.com>
2026-02-10 07:42:22 +0530

    Remove Mangum (Netlify/Lambda) adapter as Render uses standard Gunicorn

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from dotenv import load_dotenv
-from mangum import Mangum
+
 
 from .models import (
     AnalysisRequest, AnalysisResponse, 
